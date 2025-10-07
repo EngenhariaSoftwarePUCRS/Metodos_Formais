75cd90a94ca7ca4e12b6fcb8f466f03492d8c071
Felipe Freitas Silva <developer@felipefs.dev>
2025-10-06 23:23:59 -0300

    T1 - Finalizado

--- T1/T1_2025_2.thy
+++ T1/T1_2025_2.thy
@@ -2,6 +2,8 @@ theory T1_2025_2
   imports Main
 begin
 
+(* Integrantes: Carolina Ferreira, Felipe Freitas, Luiza Heller, Mateus Caçabuena *)
+
 primrec cat :: "'a list \<Rightarrow> 'a list \<Rightarrow> 'a list" where
 cateq1: "cat [] ys = ys" |
 cateq2: "cat (x#xs) ys = x#cat xs ys"
@@ -34,12 +36,57 @@ next
 qed
 
 lemma l2: "cat xs [] = xs"
-  sorry
+proof (induction xs)
+  case Nil
+  then show ?case by (simp only: cateq1)
+next
+  case (Cons h t)
+  then show ?case by (simp only: cateq2)
+qed
 
 lemma l3: "\<forall>ys :: 'a list . reverso (cat xs ys) = cat (reverso ys) (reverso xs)"
-  sorry
+proof (induction xs)
+  case Nil
+  then show ?case
+  proof (intro allI)
+    fix ys :: "'a list"
+    have "reverso (cat [] ys) = reverso ys" by (simp only: cateq1)
+    also have "... = cat (reverso ys) []" by (simp only: l2 reveq1)
+    also have "... = cat (reverso ys) (reverso [])" by (simp only: reveq1)
+    finally show "reverso (cat [] ys) = cat (reverso ys) (reverso [])" .
+  qed
+next
+  case (Cons h t)
+  then show ?case
+  proof (intro allI)
+    fix ys :: "'a list"
+    have "reverso (cat (h#t) ys) = reverso (h # cat t ys)" by (simp only: cateq2)
+    also have "... = cat (reverso (cat t ys)) [h]" by (simp only: reveq2)
+    also have "... = cat (cat (reverso ys) (reverso t)) [h]" using Cons.IH by simp
+    also have "... = cat (reverso ys) (cat (reverso t) [h])"
+      by (simp only: l1)
+    also have "... = cat (reverso ys) (reverso (h#t))" by (simp only: reveq2)
+    finally show "reverso (cat (h#t) ys) = cat (reverso ys) (reverso (h#t))" .
+  qed
+qed
+
 
 theorem t1: "reverso (reverso xs) = xs"
-  sorry
+proof (induction xs)
+  case Nil
+  then show ?case by (simp only: reveq1)
+next
+  case (Cons h t)
+  then show ?case
+  proof -
+    have "reverso (reverso (h#t)) = reverso (cat (reverso t) [h])" by (simp only: reveq2)
+    also have "... = cat (reverso [h]) (reverso (reverso t))"
+      using l3[of "reverso t"] by simp
+    also have "... = cat [h] (reverso (reverso t))" by (simp only: reveq2 reveq1 cateq1)
+    also have "... = cat [h] t" using Cons.IH by simp
+    also have "... = h#t" by (simp only: cateq2 cateq1)
+    finally show ?case .
+  qed
+qed
 
 end
